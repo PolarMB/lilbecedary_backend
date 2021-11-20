34210d12190287ca7aacdcb9eb87a7c12c08646b
Omar Vivenes <omarrgvm.97@gmail.com>
2021-11-19 23:18:17 -0300

    First commit

---
+++ src/main/java/io/lilbecedary/lilbecedary_backend/LilbecedaryBackendApplication.java
@@ -0,0 +1,13 @@
+package io.lilbecedary.lilbecedary_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilbecedaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilbecedaryBackendApplication.class, args);
+	}
+
+}
